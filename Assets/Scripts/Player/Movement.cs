408020088f22f6204f8eb3a8cc59dd4bee6dc1ff
Jesse Mitchell-Reiss <j.mitchell-reiss@outlook.com>
2023-02-14 10:12:22 -0800

    quaternions-are-fucked

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -14,6 +14,7 @@ public class Movement : MonoBehaviour
     private Vector2 movement;
     private Vector2 looking;
     private Camera cam;
+    private float angle;
     
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,7 @@ public class Movement : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         movement = Vector2.zero;
         cam = GetComponentInChildren<Camera>();
+        angle = cam.transform.localEulerAngles.x;
     }
 
     // Update is called once per frame
@@ -37,14 +39,15 @@ public class Movement : MonoBehaviour
         // rotate the character
         transform.Rotate(new Vector3(0, looking.x, 0));
         // going past max rotation
-        if (NormalizeAngles(cam.transform.localEulerAngles.x) + looking.y < minLookAngleNorm)
+        if (NormalizeAngles(angle) + looking.y < minLookAngleNorm)
         {
             looking.y = minLookAngleNorm - NormalizeAngles(cam.transform.localEulerAngles.x);
         }
-        else if (NormalizeAngles(cam.transform.localEulerAngles.x) + looking.y > maxLookAngleNorm)
+        else if (NormalizeAngles(angle) + looking.y > maxLookAngleNorm)
         {
             looking.y = maxLookAngleNorm - NormalizeAngles(cam.transform.localEulerAngles.x);
         }
+        angle -= looking.y;
         cam.transform.Rotate(-looking.y, 0, 0);
     }
 
