19963388f4307997185bc258d0a3185c27847c41
Jesse Mitchell-Reiss <j.mitchell-reiss@outlook.com>
2023-02-13 16:32:34 -0800

    initial-looking

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -8,23 +8,25 @@ public class Movement : MonoBehaviour
     private CharacterController characterController;
     private Vector2 movement;
     private Vector2 looking;
+    private Camera cam;
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         movement = Vector2.zero;
+        cam = GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0, looking.x, 0));
+        cam.transform.Rotate(looking.y, 0, 0);
     }
 
     private void FixedUpdate()
     {
         characterController.Move(new Vector3(movement.x, 0, movement.y));
-        Debug.Log(movement);
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -41,7 +43,15 @@ public class Movement : MonoBehaviour
 
     public void Look(InputAction.CallbackContext context)
     {
-        looking += context.ReadValue<Vector2>();
+        if(context.performed)
+        {
+            looking = context.ReadValue<Vector2>();
+            Debug.Log("test");
+        }
+        else if(context.canceled)
+        {
+            looking = Vector2.zero;
+        }
     }
 
     public void Fire(InputAction.CallbackContext context)
