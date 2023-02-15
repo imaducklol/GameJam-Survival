ae03cdf545f1ac860b2c7aebba1d567e9f1c6056
Owen <oschlimgen9040@student.pps.net>
2023-02-15 15:32:13 -0800

    Generation Partially Working

--- Assets/Scripts/Generation/GenerateRooms.cs
+++ Assets/Scripts/Generation/GenerateRooms.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GenerateRooms : MonoBehaviour
 {
     public GameObject player;
+    public GameObject corner;
     public GameObject noWall;
     public GameObject wall;
     public GameObject wallWithDoor;
@@ -15,19 +16,63 @@ public class GenerateRooms : MonoBehaviour
     int size;
     Block[,] grid;
 
+    int[,] blockOptions;
+
     // Start is called before the first frame update
     void Start()
     {
+        const int numberOfOptions = 32;
+
+        bool[] options;
+        int count;
+
+        blockOptions = new int[numberOfOptions,4] {
+            { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 },
+            { 1, 0, 0, 0 }, { 2, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 2, 0, 0 }, { 0, 1, 0, 0 }, { 0, 2, 0, 0 }, { 0, 1, 0, 0 }, { 0, 2, 0, 0 },
+            { 1, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 1, 1, 0 }, { 0, 1, 1, 0 }, { 0, 0, 1, 1 }, { 0, 0, 1, 1 }, { 1, 0, 0, 1 }, { 1, 0, 0, 1 },
+            { 1, 2, 0, 0 }, { 2, 1, 0, 0 }, { 0, 1, 2, 0 }, { 0, 2, 1, 0 }, { 0, 0, 1, 2 }, { 0, 0, 2, 1 }, { 2, 0, 0, 1 }, { 1, 0, 0, 2 }
+        };
         size = 2 * radius + 1;
         grid = new Block[size, size];
         for(int i = 0; i < size; i++) {
             for(int j = 0; j < size; j++) {
-                int[] walls = new int[4];
-                for(int k = 0; k < 4; k++) {
-                    walls[k] = Random.Range(0,3);
+                options = new bool[numberOfOptions];
+                count = 0;
+                for (int k = 0; k < numberOfOptions; k++) {
+                    options[k] = true;
+                    count++;
+                    if (i > 0)
+                    {
+                        if(grid[i - 1, j].wallNums[0] != blockOptions[k,2])
+                        {
+                            options[k] = false;
+                            count--;
+                        }
+                    }
+                    if (j > 0)
+                    {
+                        if (grid[i, j - 1].wallNums[1] != blockOptions[k, 3])
+                        {
+                            options[k] = false;
+                            count--;
+                        }
+                    }
                 }
-                Vector3 pos = new Vector3(gridSize * ((float)i - radius), 0f, gridSize * ((float)j - radius));
-                grid[i,j] = new Block(this, walls, pos);
+                int blockNum = Random.Range(0, count);
+                count = 0;
+                for(int k = 0; k < numberOfOptions; k++)
+                {
+                    if(options[k])
+                    {
+                        count++;
+                        if(count == blockNum)
+                        {
+                            k = numberOfOptions;
+                        }
+                    }
+                }
+                Vector3 pos = new Vector3(gridSize * (i - radius), 0f, gridSize * (j - radius));
+                grid[i, j] = new Block(this, blockOptions[0], pos);
             }
         }
     }
--- Assets/Scripts/Generation/GenerationClasses.cs
+++ Assets/Scripts/Generation/GenerationClasses.cs
@@ -4,34 +4,46 @@ using UnityEngine;
 
 public class Block
 {
-    public GameObject[] walls;
+    GameObject[] corners;
+    GameObject[] walls;
+    public int[] wallNums;
 
-    GenerateRooms script;
     float size;
     Vector3[] offsets;
 
     public Block(GenerateRooms script, int[] walls, Vector3 position)
     {
-        this.script = script;
         size = script.gridSize / 2f;
         this.walls = new GameObject[4];
+        corners = new GameObject[4];
+        wallNums = new int[4];
         offsets = new Vector3[4] { new Vector3(size, 0f, 0f), new Vector3(0f, 0f, size), new Vector3(-size, 0f, 0f), new Vector3(0f, 0f, -size) };
         for(int i = 0; i < 4; i++)
         {
-            GameObject w;
-            if(walls[i] == 0)
-            {
-                w = GameObject.Instantiate(script.noWall, script.transform);
+            corners[i] = Object.Instantiate(script.corner, script.transform);
+            corners[i].transform.position = position;
+            corners[i].transform.Rotate(0f, -90f * i, 0f);
+            if(walls[i] == 0) {
+                this.walls[i] = Object.Instantiate(script.noWall, script.transform);
             } else if(walls[i] == 1) {
-                w = GameObject.Instantiate(script.wall, script.transform);
+                this.walls[i] = Object.Instantiate(script.wall, script.transform);
             } else if (walls[i] == 2) {
-                w = GameObject.Instantiate(script.wallWithDoor, script.transform);
+                this.walls[i] = Object.Instantiate(script.wallWithDoor, script.transform);
             } else {
-                w = GameObject.Instantiate(script.noWall, script.transform);
+                this.walls[i] = Object.Instantiate(script.noWall, script.transform);
             }
-            w.transform.position = position + offsets[i];
-            w.transform.Rotate(0f, -90f * i, 0f);
-            this.walls[i] = w;
+            this.walls[i].transform.position = position + offsets[i];
+            this.walls[i].transform.Rotate(0f, -90f * i, 0f);
+            wallNums[i] = walls[i];
+        }
+    }
+
+    public void destroyGameObjects()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            Object.Destroy(corners[i]);
+            Object.Destroy(walls[i]);
         }
     }
 }
